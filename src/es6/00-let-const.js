1ccdb8f4fc531629382c7991ce0f528b29281297
David Mendoza Erazo <davidleomendoza300@gmail.com>
2023-08-10 14:44:25 -0500

    let,const,arrowFunctions,strings

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,21 @@
+var lastMame = "David";
+lastMame = "Oscar";
+console.log(lastMame);
+
+let fruit = "apple";
+fruit = "kiwi";
+console.log(fruit);
+
+const animal = "Dog";
+console.log(animal);
+const fruits = () => {
+    var fruit1 = "";
+    if(true){
+        var fruit1 = "apple";
+        let fruit2 = "kiwi"
+        const fruit3 = "Banana"
+    }
+    console.log(fruit1) 
+     
+}
+fruits();
---
+++ src/es6/03-Strings.js
@@ -0,0 +1,5 @@
+let hello = "Hello";
+let world = "world";
+// Template literals
+let totalPharse = `${hello} ${world}`
+console.log(totalPharse);
